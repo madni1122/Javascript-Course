6b8a83335b8e8cd64f854f99a7ae649c5bb75ad3
hassanabdullah40 <hassanabdullah40@gmail.com>
2026-01-21 07:44:43 +0500

    initial commit

---
+++ dem-practise/script.js
@@ -0,0 +1,15 @@
+let firstName = 'Akash';
+let firstName2 = 'Aka' + 'sh';
+
+let lastName = 'Singh';
+let userAge = 23;
+let userAge2 = 23;
+let isGraduate = false;
+let hasJob = true;
+let myNull = null;
+let myUndefined = undefined;
+let myEmptyString = '';
+
+// so all these values which are not changing throughout the web, they have specific memory address throughout all websites like we checked for false value on MDN
+
+// like false is in @75 memory address throghout all the webs
