b008f231b362e587ef25aaa6a9ee6cd4bee60dc9
Muhammad Madni <meethymeethymadni.com>
2026-03-02 17:56:29 +0500

    added more

--- dem-practise/script.js
+++ dem-practise/script.js
@@ -1,22 +1,32 @@
-// debugger;
-console.log("H-1");
-function sayHi() {
-  console.log("Hi World!");
+// function outer() {
+//   let a = 3;
+//   function parentFunc() {
+//     let b = 5;
+//     return function () {
+//       // we can even use anonymous func instead of named func
+//       console.log(a + b);
+//     };
+//   }
+//   debugger;
+//   return parentFunc();
+// }
+// const add1 = outer();
+// console.dir(add1);
+
+let a = 1;
+
+function outerFunc() {
+  let b = 2;
+
+  function innerFunc() {
+    let c = 3;
+
+    console.log(a + b + c);
+  }
+
+  return innerFunc;
 }
 
-sayHi();
-setTimeout(sayHi, 1000);
-setTimeout(function () {
-  console.log("hiii");
-}, 2000);
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
-console.log("H-2");
+let closureFunc = outerFunc();
+
+closureFunc();
