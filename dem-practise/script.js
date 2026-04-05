f86137ed04cfc41a0f68602a9b29a9ddeed8d84d
Muhammad Madni <meethymeethymadni.com>
2026-04-05 07:28:44 +0500

    added more Date: April 05, 2026 Time: 7:28am

--- dem-practise/script.js
+++ dem-practise/script.js
@@ -1,13 +1,13 @@
-debugger;
-console.log(productsData, myName, myNationality);
-import {
-  productsData,
-  myName,
-  nationality as myNationality,
-} from "./productsData.js";
-const isOrdered = false;
+const imageElem = document.querySelector("img");
+const imgLoadingElem = document.querySelector("#imgLoading");
 
-// In one line:
-// internet is a like a network of multiple devices like our smart phones, computers, desktops, smart watches etc connected through a medium like our laptops are connected through lets say fibers optics cables, or our mobiles are connected via 3G or 4G network.
-// so all these devices are connected via a network like our laptop is connected to a laptop which exists in US via fiber optics cables of network
-// so this whole net/jaal of devices connected with each other is called Internet.
+console.log();
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response) => response.json())
+  .then((data) => {
+    imageElem.src = data.message;
+    imgLoadingElem.style.display = "none";
+    imageElem.style.display = "block";
+    console.log();
+  });
