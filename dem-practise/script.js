774ad726bff180b47b79b821fdb3c11ede68ec8c
Muhammad Madni <meethymeethymadni.com>
2026-04-21 16:34:08 +0500

    promises lecture completed

--- dem-practise/script.js
+++ dem-practise/script.js
@@ -1,13 +1,28 @@
-const imageElem = document.querySelector("img");
-const imgLoadingElem = document.querySelector("#imgLoading");
+const resolveBtn = document.querySelector("#resolve-btn");
+const rejectBtn = document.querySelector("#reject-btn");
 
-console.log();
-
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((response) => response.json())
-  .then((data) => {
-    imageElem.src = data.message;
-    imgLoadingElem.style.display = "none";
-    imageElem.style.display = "block";
-    console.log();
+const p1 = new Promise((resolve, reject) => {
+  resolveBtn.addEventListener("click", () => {
+    debugger;
+    resolve("Promise Resolved");
   });
+  rejectBtn.addEventListener("click", () => {
+    debugger;
+    reject("Promise Rejected");
+  });
+});
+// p1.then((data) => {
+//   console.log(data);
+//   return "wao";
+// })
+//   .then((data) => {
+//     console.log(data);
+//   }) // we can even add more .thens. We do this in real projects using .then again and again. It makes code grow vertically, instead of horizontally.
+//   // this is the real Advantage of promises over callback hell made by xhr
+//   .catch((data) => {
+//     console.log(data);
+//   });
+const err = p1.catch(() => {
+  console.log("hi");
+});
+console.log(err);
