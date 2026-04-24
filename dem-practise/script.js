e2399a8699c43169c81adafd4f4f3771cd782e0e
Muhammad Madni <meethymeethymadni.com>
2026-04-24 17:27:07 +0500

    Fetch API

--- dem-practise/script.js
+++ dem-practise/script.js
@@ -1,28 +1,43 @@
-const resolveBtn = document.querySelector("#resolve-btn");
-const rejectBtn = document.querySelector("#reject-btn");
+// const resolveBtn = document.querySelector("#resolve-btn");
+// const rejectBtn = document.querySelector("#reject-btn");
 
-const p1 = new Promise((resolve, reject) => {
-  resolveBtn.addEventListener("click", () => {
-    debugger;
-    resolve("Promise Resolved");
-  });
-  rejectBtn.addEventListener("click", () => {
-    debugger;
-    reject("Promise Rejected");
-  });
-});
-// p1.then((data) => {
-//   console.log(data);
-//   return "wao";
-// })
-//   .then((data) => {
-//     console.log(data);
-//   }) // we can even add more .thens. We do this in real projects using .then again and again. It makes code grow vertically, instead of horizontally.
-//   // this is the real Advantage of promises over callback hell made by xhr
-//   .catch((data) => {
-//     console.log(data);
+// const p1 = new Promise((resolve, reject) => {
+//   resolveBtn.addEventListener("click", () => {
+//     debugger;
+//     resolve("Promise Resolved");
 //   });
-const err = p1.catch(() => {
-  console.log("hi");
-});
-console.log(err);
+//   rejectBtn.addEventListener("click", () => {
+//     debugger;
+//     reject("Promise Rejected");
+//   });
+// });
+// // p1.then((data) => {
+// //   console.log(data);
+// //   return "wao";
+// // })
+// //   .then((data) => {
+// //     console.log(data);
+// //   }) // we can even add more .thens. We do this in real projects using .then again and again. It makes code grow vertically, instead of horizontally.
+// //   // this is the real Advantage of promises over callback hell made by xhr
+// //   .catch((data) => {
+// //     console.log(data);
+// //   });
+// const err = p1.catch(() => {
+//   console.log("hi");
+// });
+// console.log(err);
+
+// Fetch API  Better than XHR
+
+fetch("https://dummyjson.co/products/add", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Apple Watch",
+    type: "Gadget",
+  }),
+})
+  .then((res) => res.json())
+  .then(console.log)
+  .catch((err) => {
+    console.log(err);
+  });
